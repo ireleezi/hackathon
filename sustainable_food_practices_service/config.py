d04f1147fa8b97386204f4a3ea88f74fcb6073b8
BHarris02 <118061899+BHarris02@users.noreply.github.com>
2024-10-05 16:41:44 +0100

    add sustainable_food_practices_service

---
+++ sustainable_food_practices_service/config.py
@@ -0,0 +1,4 @@
+
+class config:
+    PARKS_DATASET_URL = 'https://www.belfastcity.gov.uk/getmedia/20388c00-dfd5-4fd4-83aa-73870a1b6eb6/parksdata.csv'
+    TOWNLANDS_DATASET_URL = '' #THis is not a dataset
---
+++ sustainable_food_practices_service/utils.py
@@ -0,0 +1,18 @@
+from config import config
+import pandas as pd
+import requests
+from io import StringIO
+
+def fetch_onidata() -> pd.DataFrame:
+    try:
+        resp = requests.get(config.PARKS_DATASET_URL)
+        resp.raise_for_status()
+        data = StringIO(resp.text)
+        return pd.read_csv(data, skipinitialspace=True, quotechar='"')
+    except Exception as e:
+        print(f"Error fetching ONI Data: {e}")
+        return None
+    
+def process_data(df: pd.DataFrame) -> pd.DataFrame:
+    #ai model logic
+    return df['NAME'].to_dict()
